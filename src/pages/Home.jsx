2a2918b212bc3dc298d322fbbb7bdfea7a98b783
Idham Ganteng <idham16iramap.com>
2025-05-20 22:46:07 +0700

    update today 20 mei 2025

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,21 @@
+import { AboutSection } from "../components/AboutSection";
+import { HeroSection } from "../components/HeroSections";
+import { Navbar } from "../components/Navbar";
+import { StarBackground } from "../components/StarBackground";
+import { ThemeToogle } from "../components/ThemeToogle";
+
 export const HomePage = () => {
   return (
-    <div className="min-h-screen" >
+    <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
       {/* Theme Toogle */}
-
+      <ThemeToogle />
       {/*Background Effects*/}
-
+      <StarBackground />
       {/*Navbar*/}
-
+      <Navbar />
       {/*Main Context*/}
-
+      <HeroSection />
+      <AboutSection/>
       {/*Footer*/}
     </div>
   );
